7576d6fdbcdd0a327683a92f1ca39f8227766208
Steve Giles <stephan.giles@gmail.com>
2021-06-12 08:04:51 -0400

    fix references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
--- src/app/filters/filters.module.ts
+++ src/app/filters/filters.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FiltersComponent } from './filters/filters.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
 
 
 
